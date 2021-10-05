8392ef9549b450c12c890622e7599dfe6d0973bd
Josué Alves <jow17alves@gmail.com>
2021-10-05 15:51:12 -0300

    diagonal pela direita pronto

--- assets/js/conditionWin.js
+++ assets/js/conditionWin.js
@@ -1,3 +1,12 @@
+let teste = [
+    "WWWWWWW",
+    "WWWWRWW",
+    "WWWRWWW",
+    "WWRWWWW",
+    "WRWWWWW",
+    "WWWWWWW"
+];
+
 function victory(array) {
 
     const modColumns = array[1].length - 3;
@@ -12,13 +21,13 @@ function victory(array) {
             if (array[i][j] == "B") {
                 
                 if (array[i][j] == array[i][j + 1] && array[i][j] == array[i][j + 2] && array[i][j] == array[i][j + 3]) {
-                    alert("vitoria horizontal do preto")
+                    console.log("vitoria horizontal do preto")
                 }
             }
             if (array[i][j] == "R") {
                 
                 if (array[i][j] == array[i][j + 1] && array[i][j] == array[i][j + 2] && array[i][j] == array[i][j + 3]) {
-                    alert("vitoria horizontal do vermelho")
+                    console.log("vitoria horizontal do vermelho")
                 }
             }
         }
@@ -47,5 +56,43 @@ function victory(array) {
     }
 
     //vitoria diagonal 
+
+
+    for(let i=0;i<modLines;i++){
+        
+        for(let j=0;j<modColumns;j++){
+            
+             if(array[i][j] == "B"){
+                 
+                    if(array[i][j] == array[i+1][j+1] && array[i][j] == array[i+2][j+2] && array[i][j] == array[i+3][j+3]){
+                        alert("vitoria diagonal direita do preto")
+                    }
+             }
+             if(array[i][j] == "R"){
+                if(array[i][j] == array[i+1][j+1] && array[i][j] == array[i+2][j+2] && array[i][j] == array[i+3][j+3]){
+                    alert("vitoria diagonal direita do vermelho")
+                }
+             }
+        }
+    }
+
+    for(let i=0;i<modLines;i++){
+        
+        for(let j=0;j<modColumns;j++){
+            
+             if(array[i][j] == "B"){
+                 
+                    if(array[i][j] == array[i+1][j-1] && array[i][j] == array[i+2][j-2] && array[i][j] == array[i+3][j-3]){
+                        alert("vitoria diagonal esquerda do preto")
+                    }
+             }
+             if(array[i][j] == "R"){
+                 console.log("cheguei aqui")
+                if(array[i][j] == array[i+1][j-1] && array[i][j] == array[i+2][j-2] && array[i][j] == array[i+3][j-3]){
+                    alert("vitoria diagonal esquerda do vermelho")
+                }
+             }
+        }
+    }
 
     }
