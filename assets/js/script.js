19af619e617b16de4d5df21acc65cac0b877dbb8
nicholas <nicholassantos2017@gmail.com>
2021-10-05 14:40:19 -0300

    arrumando código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,3 @@
-//aqui cria a div pai das peças
-const body = document.querySelector('body')
-const pieces = document.createElement('div')
-pieces.classList.add("pieces")
-
-body.appendChild(pieces)
-
-/* ---------------------------------------------- */
-
-//aqui cria as funções para a construção das peças
-function creatorOne(){
-    const playerOne = document.createElement('div')
-    playerOne.classList.add("playerOne")
-
-    pieces.appendChild(playerOne)
-}
-creatorOne()//teste
-
-function creatorTwo(){
-    const playerTwo = document.createElement('div')
-    playerTwo.classList.add("playerTwo")
-
-    pieces.appendChild(playerTwo)
-}
-creatorTwo()//teste
-/* ------------------------------------------------- */
-
-//aqui adiciona o listener à div ref à coluna
-document.addEventListener("click", function(){
-    //fazer um "toggle" on/off para alternar entre os players 1 e 2
-    //selecionar a coluna clicada e chamar a função creator de acordo com o status
-    //do toggle
-})
-
 const mainContainer = document.getElementById('mainContainer');
 
 const board = [
@@ -69,5 +35,40 @@ function createBoard(){
 }
 createBoard()
 
-//Fim da criação do tabuleiro
+
+//aqui cria a div pai das peças
+const body = document.querySelector('body')
+const pieces = document.createElement('div')
+pieces.classList.add("pieces")
+
+body.appendChild(pieces)
+
+/* ---------------------------------------------- */
+
+//aqui cria as funções para a construção das peças
+function creatorOne(){
+    const playerOne = document.createElement('div')
+    playerOne.classList.add("playerOne")
+
+    pieces.appendChild(playerOne)
+}
+creatorOne()//teste
+
+function creatorTwo(){
+    const playerTwo = document.createElement('div')
+    playerTwo.classList.add("playerTwo")
+
+    pieces.appendChild(playerTwo)
+}
+creatorTwo()//teste
+/* ------------------------------------------------- */
+
+//aqui adiciona o listener à div ref à coluna
+document.addEventListener("click", function(){
+    //fazer um "toggle" on/off para alternar entre os players 1 e 2
+    //selecionar a coluna clicada e chamar a função creator de acordo com o status
+    //do toggle
+})
+
+
 
