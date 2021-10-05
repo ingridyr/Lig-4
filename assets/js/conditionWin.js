919633edec01f15861ced511187c4db3e7458208
Josué Alves <jow17alves@gmail.com>
2021-10-05 14:23:24 -0300

    condiçoes de vitoria horizontal e vertical criadas

---
+++ assets/js/conditionWin.js
@@ -0,0 +1,51 @@
+function victory(array) {
+
+    const modColumns = array[1].length - 3;
+    const modLines = array.length - 3;
+
+        //vitoria horizontal
+
+    for (let i = 0; i < array.length; i++) {
+
+        for (let j = 0; j < modColumns; j++) {
+
+            if (array[i][j] == "B") {
+                
+                if (array[i][j] == array[i][j + 1] && array[i][j] == array[i][j + 2] && array[i][j] == array[i][j + 3]) {
+                    alert("vitoria horizontal do preto")
+                }
+            }
+            if (array[i][j] == "R") {
+                
+                if (array[i][j] == array[i][j + 1] && array[i][j] == array[i][j + 2] && array[i][j] == array[i][j + 3]) {
+                    alert("vitoria horizontal do vermelho")
+                }
+            }
+        }
+
+    }
+
+    //vitoria vertical
+
+    for (let i = 0; i < modLines; i++) {
+
+        for (let j = 0; j < array[1].length; j++) {
+
+            if (array[i][j] == "B") {
+
+                if (array[i][j] == array[i + 1][j] && array[i][j] == array[i + 2][j] && array[i][j] == array[i + 3][j]) {
+                    alert("vitoria vertical do preto")
+                }
+            }
+            if (array[i][j] == "R") {
+
+                if (array[i][j] == array[i + 1][j] && array[i][j] == array[i + 2][j] && array[i][j] == array[i + 3][j]) {
+                   alert("vitoria vertical do vermelho")
+                }
+            }
+        }
+    }
+
+    //vitoria diagonal 
+
+    }
