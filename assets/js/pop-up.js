70b5ed06585ab4e7d375931b9c45cb2c0f725637
Josué Alves <jow17alves@gmail.com>
2021-10-06 13:58:40 -0300

    arquivo pop-up.js criado

---
+++ assets/js/pop-up.js
@@ -0,0 +1,9 @@
+function popUp(){
+    const body=document.querySelector("body")
+    const div=document.createElement("div")
+    const paragraph=document.createElement("p")
+    body.appendChild(div)
+    div.appendChild(paragraph)
+
+    
+}
