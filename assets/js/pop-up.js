8f6dc90af32b5aad2159755a95e4c741fae3a286
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2021-10-06 16:04:57 -0300

    adiciona popUp para cada jogador

--- assets/js/pop-up.js
+++ assets/js/pop-up.js
@@ -1,9 +1,28 @@
-function popUp(){
-    const body=document.querySelector("body")
-    const div=document.createElement("div")
-    const paragraph=document.createElement("p")
+
+function popUpPlayer1(){
+    const body      = document.querySelector("body")
+    const div       = document.createElement("div")
+    div.className   = "popUpPlayer1"
+    const paragraph = document.createElement("p")
+
+    div.appendChild(paragraph)
+    body.appendChild(div)
+}
+popUpPlayer1()
+
+
+function popUpPlayer2(){
+    const body      = document.querySelector("body")
+    const div       = document.createElement("div")
+    div.className   = "popUpPlayer2"
+
+    const paragraph = document.createElement("p")
+
     body.appendChild(div)
     div.appendChild(paragraph)
-
-    
 }
+popUpPlayer2()
+
+//substituir o alert
+//tem que ter um para o B e um para o R
+//if adiciona uma classe
