1b89b059faf152943d8a6ab188f5d36d47834386
Douglas Alencar <doug.aluz@gmail.com>
2021-10-07 13:47:23 -0300

    Alterando board

--- assets/js/moviment.js
+++ assets/js/moviment.js
@@ -46,31 +46,62 @@ col6.forEach(function(c){
 c.addEventListener('click', addPiece);
 })
 
+// const l0 = document.querySelector('.rowContainer0')
+// l0.addEventListener('click', addPiece)
 
 
 
-let clic = true
+
+
+
+let board1 = [
+    [0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,]
+]
+
+
+let player = 1
+let click = true
+
 function addPiece(evt){
     let column = evt.target
-    console.log(evt)
+    let id = evt.target.id
+    
+
+
+    if (click){
+        click = false
+        player = 2
+    }else{
+        click = true
+        player = 1
+    }
+
+    for(let i = board1.length-1; i>=0; i--){
+        if(board1[i][id]===0){
+            board1[i][id] = player
+            break
+        }
+    }
+console.log(board1)
 
 
 
+        // if(click ){
+        //     let piece = creatorOne()
+        //     //for(let i = board1.lengh-1; i>=0; i--){}
+            
+        //     column.appendChild(piece)
+        //     click = false
+            
+        // }else if(click === false && evt.srcElement.childNodes.length === 0){
+        //     let piece = creatorTwo()
+        //     column.appendChild(piece)
+        //     click = true
+        // }
 
-if(clic && evt.srcElement.childNodes.length === 0){
-    let piece = creatorOne()
-    column.appendChild(piece)
-    clic = false
-}else if(clic === false && evt.srcElement.childNodes.length === 0){
-    let piece = creatorTwo()
-    column.appendChild(piece)
-    clic = true
-}
-
-}
-let a = document.querySelector('.rowContainer1')
-a.addEventListener('click', b)
-
-function b(evt){
-    console.log(evt)
 }
